ef6bff20f058bb543323e9463d374bc15bb5247c
dfredes <101341795+dfredes@users.noreply.github.com>
2022-11-11 11:46:53 -0300

    api?

--- Asisterin/src/app/nombre-servicio.service.ts
+++ Asisterin/src/app/nombre-servicio.service.ts
@@ -4,6 +4,23 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class NombreServicioService {
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin' :'*'
+    })
+  }
 
-  constructor() { }
+  apiURL = 'https://my-json-server.typicode.com/victorrosendo/repoUsuariosRamos';
+
+  constructor(private http:HttpClient) { }
+
+
+  getUsuarios():Observable<any>{
+    return this.http.get(this.apiURL+'/users/').pipe(
+      retry(3)
+    );
+  }
+
+
 }
