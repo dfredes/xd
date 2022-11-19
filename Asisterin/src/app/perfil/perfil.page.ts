53137b0013b415659ef8aa38798d94da77b6dec3
dfredes <101341795+dfredes@users.noreply.github.com>
2022-11-19 02:11:41 -0300

    camara?

--- Asisterin/src/app/perfil/perfil.page.ts
+++ Asisterin/src/app/perfil/perfil.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CameraPreview, CameraPreviewPictureOptions, CameraPreviewOptions, CameraPreviewDimensions } from '@awesome-cordova-plugins/camera-preview/ngx';
+import { Camera, CameraOptions } from '@awesome-cordova-plugins/camera/ngx';
 
 @Component({
   selector: 'app-perfil',
@@ -7,10 +9,89 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilPage implements OnInit {
 
-  constructor() { }
+  constructor(private cameraPreview: CameraPreview,private camera: Camera) { }
 
   ngOnInit() {
-    //hacer aqui la wea del camara
-  }
 
-}
+    // camera options (Size and location). In the following example, the preview uses the rear camera and display the preview in the back of the webview
+    const cameraPreviewOpts: CameraPreviewOptions = {
+      x: 0,
+      y: 0,
+      width: window.screen.width,
+      height: window.screen.height,
+      camera: 'rear',
+      tapPhoto: true,
+      previewDrag: true,
+      toBack: true,
+      alpha: 1,
+      storeToFile: false
+    }
+
+    // start camera
+    this.cameraPreview.startCamera(cameraPreviewOpts).then(
+      (res) => {
+        console.log(res)
+      },
+      (err) => {
+        console.log(err)
+      });
+
+    // Set the handler to run every time we take a picture
+    this.cameraPreview.setOnPictureTakenHandler().subscribe((result) => {
+      console.log(result);
+      // do something with the result
+    });
+
+
+    // picture options
+    const pictureOpts: CameraPreviewPictureOptions = {
+      width: 1280,
+      height: 1280,
+      quality: 85
+    }
+
+    // take a picture
+    this.cameraPreview.takePicture(this.pictureOpts).then((imageData) => {
+      this.picture = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      console.log(err);
+      this.picture = 'assets/img/test.jpg';
+    });
+
+    // take a snap shot
+    this.cameraPreview.takeSnapshot(this.pictureOpts).then((imageData) => {
+      this.picture = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+      console.log(err);
+      this.picture = 'assets/img/test.jpg';
+    });
+
+
+    // Switch camera
+    this.cameraPreview.switchCamera();
+
+    // set color effect to negative
+    this.cameraPreview.setColorEffect('negative');
+
+    // Stop the camera preview
+    this.cameraPreview.stopCamera();
+
+
+
+        const options: CameraOptions = {
+          quality: 100,
+          destinationType: this.camera.DestinationType.FILE_URI,
+          encodingType: this.camera.EncodingType.JPEG,
+          mediaType: this.camera.MediaType.PICTURE
+        }
+        
+        this.camera.getPicture(options).then((imageData) => {
+        // imageData is either a base64 encoded string or a file URI
+        // If it's base64 (DATA_URL):
+        let base64Image = 'data:image/jpeg;base64,' + imageData;
+        }, (err) => {
+        // Handle error
+        });
+      }
+
+    }
